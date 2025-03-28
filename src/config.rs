6524186268c9ed3288eace99ce50cfeb9d5ef387
lu-notebook <904241188@qq.com>
2025-03-28 23:11:41 +0800

    修改配置华为云服务器

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,10 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];  // 默认服务器
+pub const RENDEZVOUS_SERVERS: &[&str] = &["113.45.24.8"];
+// pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";  // 默认密钥
+pub const RS_PUB_KEY: &str = "N997puxaHobzPmBi2PRFfTLFg7gCdAX9YdgAmd9H0bI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
